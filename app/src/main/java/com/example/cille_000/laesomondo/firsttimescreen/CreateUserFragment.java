8f09dd5041669a8b411fb2e8b3c6dd7bff8c5021
Sofie Larsen <sofie_p_larsen@hotmail.com>
2016-10-31 11:17:23 +0100

    Rettelser

--- app/src/main/java/com/example/cille_000/laesomondo/firsttimescreen/CreateUserFragment.java
+++ app/src/main/java/com/example/cille_000/laesomondo/firsttimescreen/CreateUserFragment.java
@@ -1,9 +1,20 @@
 package com.example.cille_000.laesomondo.firsttimescreen;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.cille_000.laesomondo.R;
 
 public class CreateUserFragment extends Fragment {
 
+    TextView text;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.activity_start, container, false);
+
+    }
 }
--- app/src/main/java/com/example/cille_000/laesomondo/firsttimescreen/StartActivity.java
+++ app/src/main/java/com/example/cille_000/laesomondo/firsttimescreen/StartActivity.java
@@ -1,16 +1,27 @@
 package com.example.cille_000.laesomondo.firsttimescreen;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.cille_000.laesomondo.R;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends FragmentActivity {
+
+    Fragment createUser;
+    Fragment testInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+//        FragmentActivity activity = new FragmentActivity();
+//        android.app.FragmentManager manager = activity.getFragmentManager();
+//        manager.getFragment(createUser, );
+
 
     }
 }
