7a020332af335851a0025e918097b64423d3c99f
Sofie Larsen <sofie_p_larsen@hotmail.com>
2016-11-14 09:02:43 +0100

    Flere ændringer

--- app/src/main/java/com/example/cille_000/laesomondo/firsttimescreen/TestInfoFragment.java
+++ app/src/main/java/com/example/cille_000/laesomondo/firsttimescreen/TestInfoFragment.java
@@ -5,15 +5,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.cille_000.laesomondo.R;
 
-public class TestInfoFragment extends Fragment {
+public class TestInfoFragment extends Fragment implements View.OnClickListener {
+
+    private TextView notnow;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_testinfo, container, false);
 
+        notnow = (TextView) view.findViewById(R.id.testinfo_notnow);
+        notnow.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == notnow)
+        {
+            System.out.println("NOT NOW");
+        }
     }
 }
