534a4720b1c7f31d941ceed6c9fd91acf4857e13
Sofie Larsen <sofie_p_larsen@hotmail.com>
2016-11-29 15:43:42 +0100

    Rettelser

--- app/src/main/java/com/example/cille_000/laesomondo/mainscreen/MainActivity.java
+++ app/src/main/java/com/example/cille_000/laesomondo/mainscreen/MainActivity.java
@@ -4,51 +4,51 @@ package com.example.cille_000.laesomondo.mainscreen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 import com.example.cille_000.laesomondo.R;
 import com.example.cille_000.laesomondo.challengescreen.TextInfoActivity;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageButton burgerButton, userButton;
     private ArrayList<ImageButton> coverList;
-
-
+    private UserProfileFragment userprofile;
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //comment
-        //this should remove the statusbar
-        //  requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //  this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        // Find the toolbar view inside the activity layout
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        userprofile = new UserProfileFragment();
         coverList = new ArrayList<>();
 
-
-
         userButton = (ImageButton) findViewById(R.id.userbutton);
+        title = (TextView) findViewById(R.id.toolbar_title);
         burgerButton = (ImageButton) findViewById(R.id.burgerbutton);
         coverList.add((ImageButton) findViewById(R.id.imageButtonCover1));
         coverList.add((ImageButton) findViewById(R.id.imageButtonCover2));
         coverList.add((ImageButton) findViewById(R.id.imageButtonCover3));
         coverList.add((ImageButton) findViewById(R.id.imageButtonCover4));
 
-
         userButton.setOnClickListener(this);
         burgerButton.setOnClickListener(this);
+
         for (ImageButton i: coverList) {
             i.setOnClickListener(this);
         }
@@ -56,7 +56,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -70,12 +69,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
             }
         }
-//        if (v == burgerButton) {
-//
-//        }
-        if (v == userButton) {
-            Intent intent = new Intent(this, UserProfileActivity.class);
-            startActivity(intent);
+        if(v == burgerButton) {
+            if(userprofile != null && userprofile.isVisible()) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.remove(userprofile).commit();
+
+                burgerButton.setBackground(getResources().getDrawable(R.drawable.burgerbutton01));
+                userButton.setVisibility(View.VISIBLE);
+                title.setText(getResources().getString(R.string.MainScreenTitle));
+            }
+
+        }
+        else if (v == userButton) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.main_framelayout, userprofile);
+            transaction.commit();
+
+            burgerButton.setBackground(getResources().getDrawable(R.drawable.backarrow));
+            userButton.setVisibility(View.INVISIBLE);
+            title.setText(getResources().getString(R.string.MainScreenUserProfile));
         }
     }
 }
---
+++ app/src/main/java/com/example/cille_000/laesomondo/mainscreen/UserProfileFragment.java
@@ -0,0 +1,47 @@
+package com.example.cille_000.laesomondo.mainscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.cille_000.laesomondo.R;
+
+
+public class UserProfileFragment extends Fragment implements View.OnClickListener {
+
+    private TextView lixvalue, wordMinValue;
+    private ImageButton achievement, stats, backArrow;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_userprofile, container, false);
+
+        wordMinValue = (TextView) view.findViewById(R.id.userprofile_wordminvalue);
+        lixvalue = (TextView) view.findViewById(R.id.userprofile_lixvalue);
+        achievement = (ImageButton) view.findViewById(R.id.userprofile_achievement);
+        stats = (ImageButton) view.findViewById(R.id.userprofile_stats);
+
+        achievement.setOnClickListener(this);
+        stats.setOnClickListener(this);
+
+        return view;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+//        if(v == achievement) {
+//        }
+//
+//        if(v == stats) {
+//        }
+    }
+}
