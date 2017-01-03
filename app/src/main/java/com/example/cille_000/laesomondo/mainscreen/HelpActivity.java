5df894d349b727f525ea16604e3a1900e25afe36
Sofie Larsen <sofie_p_larsen@hotmail.com>
2017-01-03 11:02:15 +0100

    Tilføjet tre activities, ændringer i manifest

---
+++ app/src/main/java/com/example/cille_000/laesomondo/mainscreen/HelpActivity.java
@@ -0,0 +1,12 @@
+package com.example.cille_000.laesomondo.mainscreen;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+
+public class HelpActivity extends Fragment implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/example/cille_000/laesomondo/mainscreen/MenuFragment.java
+++ app/src/main/java/com/example/cille_000/laesomondo/mainscreen/MenuFragment.java
@@ -1,26 +1,44 @@
 package com.example.cille_000.laesomondo.mainscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.cille_000.laesomondo.R;
 
 public class MenuFragment extends Fragment implements View.OnClickListener {
 
-
+    private TextView settings, help, contact;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        settings = (TextView) view.findViewById(R.id.menuSettings);
+        help = (TextView) view.findViewById(R.id.menuHelp);
+        contact = (TextView) view.findViewById(R.id.menuContact);
+
+        settings.setOnClickListener(this);
+        help.setOnClickListener(this);
+        contact.setOnClickListener(this);
 
         return view;
     }
 
     @Override
     public void onClick(View v) {
+        if(v == settings) {
+            Intent intent = new Intent(getActivity(), SettingsActivity.class);
+            startActivity(intent);
+        } else if(v == help) {
+
+        } else if(v == contact) {
+
+        }
 
     }
 }
