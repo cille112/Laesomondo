111504eec7321f7405fbf7f44e486d06670d2359
Bulqe <Burima@hotmail.com>
2017-01-03 11:43:54 +0100

    Created AchievementFragmen, fragment_achievement. changed UserProfileFragment, strings.

--- app/src/main/java/com/example/cille_000/laesomondo/mainscreen/UserProfileFragment.java
+++ app/src/main/java/com/example/cille_000/laesomondo/mainscreen/UserProfileFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -19,10 +20,14 @@ public class UserProfileFragment extends Fragment implements View.OnClickListene
 
     private TextView lixvalue, wordMinValue;
     private ImageButton achievement, stats;
+    private AchievementFragment achievementscreen;
+   // private StatsFragment Statsscreen;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_userprofile, container, false);
+        achievementscreen = new AchievementFragment();
+       // statsscreen = new StatsFragment();
 
         wordMinValue = (TextView) view.findViewById(R.id.userprofile_wordminvalue);
         lixvalue = (TextView) view.findViewById(R.id.userprofile_lixvalue);
@@ -38,10 +43,16 @@ public class UserProfileFragment extends Fragment implements View.OnClickListene
 
     @Override
     public void onClick(View v) {
-//        if(v == achievement) {
-//        }
-//
+        if(v == achievement) {
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.main_framelayout, achievementscreen);
+            transaction.commit();
+        }
+
 //        if(v == stats) {
+//            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//            transaction.replace(R.id.main_framelayout, statsscreen);
+//            transaction.commit();
 //        }
     }
 }
