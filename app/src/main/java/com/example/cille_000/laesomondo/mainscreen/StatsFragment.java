ddc43f230c0a55b59fa04627d6ca31a107eaa274
Bulqe <Burima@hotmail.com>
2017-01-03 12:21:57 +0100

    Changed StatsFragment, UserProfileFragment, fragment_stats, strings. uploaded fff.

--- app/src/main/java/com/example/cille_000/laesomondo/mainscreen/StatsFragment.java
+++ app/src/main/java/com/example/cille_000/laesomondo/mainscreen/StatsFragment.java
@@ -1,6 +1,26 @@
 package com.example.cille_000.laesomondo.mainscreen;
 
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 
-public class StatsFragment {
+import com.example.cille_000.laesomondo.R;
+
+public class StatsFragment extends Fragment {
+
+    private ImageButton stats;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_stats, container, false);
+
+
+        stats = (ImageButton) view.findViewById(R.id.StatsTMP);
+
+        return view;
+    }
 }
