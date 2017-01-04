1a5a0964795eeefcc86ac585d9178e2e35f44bab
Bulqe <Burima@hotmail.com>
2017-01-04 13:39:53 +0100

    Changed statsFragmen, fragment_stats build.gradle added radar chart

--- app/src/main/java/com/example/cille_000/laesomondo/mainscreen/StatsFragment.java
+++ app/src/main/java/com/example/cille_000/laesomondo/mainscreen/StatsFragment.java
@@ -6,20 +6,71 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 
 import com.example.cille_000.laesomondo.R;
+import com.numetriclabz.numandroidcharts.ChartData;
+import com.numetriclabz.numandroidcharts.RadarChart;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class StatsFragment extends Fragment {
 
-    private ImageButton stats;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_stats, container, false);
 
+        RadarChart radarChart = (RadarChart) view.findViewById(R.id.radar_chart);
 
-        stats = (ImageButton) view.findViewById(R.id.StatsTMP);
+
+        List<ChartData> values = new ArrayList<>();
+
+        ArrayList<String> label = new ArrayList();
+        label.add("Speed");
+        label.add("concentration");
+        label.add("Words/min");
+        label.add("Lix");
+        label.add("Punctuality");
+        label.add("Problem");
+
+        ArrayList<Float> entries = new ArrayList<>();
+        entries.add(2f);
+        entries.add(2f);
+        entries.add(2f);
+        entries.add(2f);
+        entries.add(4f);
+        entries.add(1f);
+
+        ArrayList<Float> entries2 = new ArrayList<>();
+        entries2.add(1f);
+        entries2.add(1f);
+        entries2.add(1f);
+        entries2.add(1f);
+        entries2.add(4f);
+        entries2.add(6f);
+
+
+        try {
+            JSONObject dataSet = new JSONObject();
+            dataSet.put("labels", label.toString());
+
+            JSONObject val = new JSONObject();
+            val.put("You", entries.toString());
+            val.put("Average", entries2.toString());
+
+            dataSet.put("values", val.toString());
+
+            values.add(new ChartData(dataSet));
+            radarChart.setData(values);
+        }
+        catch (JSONException e){
+            e.printStackTrace();
+        }
 
         return view;
     }
