5f49e3d5b9e7b479e3a2dbbe76d059a91f76af3c
Sofie Larsen <sofie_p_larsen@hotmail.com>
2017-01-05 15:26:56 +0100

    Settingsfragment implementeret

--- app/src/main/java/com/example/cille_000/laesomondo/mainscreen/SettingsFragment.java
+++ app/src/main/java/com/example/cille_000/laesomondo/mainscreen/SettingsFragment.java
@@ -6,17 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.cille_000.laesomondo.R;
 
 
-public class SettingsFragment extends Fragment implements View.OnClickListener {
+public class SettingsFragment extends Fragment implements View.OnClickListener, SeekBar.OnSeekBarChangeListener{
+
+    private SeekBar textSize;
+    private TextView currentText;
 
     public SettingsFragment() {
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        textSize = (SeekBar) view.findViewById(R.id.textsizebar);
+        currentText = (TextView) view.findViewById(R.id.currenttextsize);
+        textSize.setOnSeekBarChangeListener(this);
 
         return view;
     }
@@ -39,6 +48,30 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        float size = 16;
+
+        if(progress == 0) {
+            size = 16;
+        } else {
+            size += progress / 5;
+        }
+
+        currentText.setTextSize(size);
+        currentText.setText("" + size);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
 }
