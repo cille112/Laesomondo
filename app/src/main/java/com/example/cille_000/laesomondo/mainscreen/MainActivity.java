24724130d3784ab5b579b0438939eb82614a16b0
JonesBawz <jonasfriis94@gmail.com>
2017-01-18 14:58:13 +0100

    Main is now waiting for textread to be downloaded by firebase

--- app/src/main/java/com/example/cille_000/laesomondo/mainscreen/MainActivity.java
+++ app/src/main/java/com/example/cille_000/laesomondo/mainscreen/MainActivity.java
@@ -251,6 +251,7 @@ public class MainActivity extends ActionBarActivity implements DrawerFragment.Dr
 
                     if(!isCancelled()) {
                         int i = 0;
+                        //wait for the following
                         while (genre==null && textRead == null) {
                             this.wait(500);
                             System.out.println(i);
